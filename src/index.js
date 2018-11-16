8723ba6e84a527cfeba36f2d503fa8532f6d615d
will2dev <wylliam.bastiani@gmail.com>
2018-11-16 16:24:24 -0200

    initial scaffolding

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles/boomers-iconized-menu.css';
+
+class Fancy extends React.Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+export default Fancy;
